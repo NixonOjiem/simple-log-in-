ae4aa439df38f52d151a604009e20b22e280fb57
arkstudiodevelopers <nicksonogm@gmail.com>
2024-12-03 15:45:30 +0300

    Added log out functionality

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserLarge } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 
 function Header() {
     const [username, setUsername] = useState('');
+    const navigate = useNavigate(); // Initialize useHistory
 
     useEffect(() => {
       const storedUsername = localStorage.getItem('username');
@@ -11,11 +13,18 @@ function Header() {
         setUsername(storedUsername);
       }
     }, []);
+
+    const handleLogout = () => {
+      localStorage.removeItem('username'); // Clear the username from local storage
+      setUsername(''); // Clear the username state
+      navigate('/signin'); // Redirect to the login page
+  };
   
     return (
       <div className='Header'>
         <FontAwesomeIcon icon={faUserLarge} className='UserIcon'/>
         <h1 className='Username'>{username && <p>Hello, {username}!</p>}</h1>
+        <p className='LogOut' onClick={handleLogout}>Log Out</p>
       </div>
     );
   };
