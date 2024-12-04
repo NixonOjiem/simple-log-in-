40480554ff9b78a6892f5e98baeedb3a0c41cc2e
arkstudiodevelopers <nicksonogm@gmail.com>
2024-12-04 20:44:28 +0300

    Styled the Left Navigation bar

--- frontend/src/components/LeftNavigation.js
+++ frontend/src/components/LeftNavigation.js
@@ -2,14 +2,58 @@ import React from 'react'
 
 function LeftNavigation() {
   return (
-    <div className='Leftnavigation'>
-        <h2>Test</h2>
-        <ul className='Month-list'>
-            <li>Test 1</li>
-            <li>Test 2</li>
-            <li>Test 3</li>
-        </ul>
+    
+    <div id="nav-bar">
+      <input id="nav-toggle" type="checkbox" />
+      <div id="nav-header">
+        <a id="nav-title" href="https://codepen.io" target="_blank">
+        Q<i class="fab fa-codepen"></i>uizQuest
+        </a>
+        <label for="nav-toggle">
+        <span id="nav-toggle-burger"></span>
+        </label>
+         <hr />
+      </div>
+      <div id="nav-content">
+        <div class="nav-button">
+          <i class="fas fa-palette"></i>
+          <span>Anime Quize</span>
+        </div>
+        <div class="nav-button">
+          <i class="fas fa-images"></i>
+          <span>History Quiz</span>
+        </div>
+        <div class="nav-button">
+          <i class="fas fa-thumbtack"></i>
+          <span>Random Quiz</span>
+       </div>
+        <hr />
+    <div class="nav-button">
+      <i class="fas fa-heart"></i>
+      <span>Quiz History</span>
     </div>
+    <div class="nav-button">
+      <i class="fas fa-chart-line"></i>
+      <span>TBD</span>
+    </div>
+    <div class="nav-button">
+      <i class="fas fa-fire"></i>
+      <span>TBD</span>
+    </div>
+    <div class="nav-button">
+      <i class="fas fa-magic"></i>
+      <span>TBD</span>
+    </div>
+    <hr />
+    <div class="nav-button">
+      <i class="fas fa-gem"></i>
+      <span>By Nixon Ojiem</span>
+    </div>
+    <div id="nav-content-highlight"></div>
+  </div>
+  <input id="nav-footer-toggle" type="checkbox" />
+ 
+</div>
   )
 }
 
