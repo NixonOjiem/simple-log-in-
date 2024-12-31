fae8eb4c9dfc9de58c0ba8c0f87f5e36d57f1b13
arkstudiodevelopers <nicksonogm@gmail.com>
2024-12-31 19:04:06 +0300

    started working on some of the front end for user ranking

--- frontend/src/components/UserRanking.js
+++ frontend/src/components/UserRanking.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useEffect, useState} from 'react'
 
 const UserRanking=()=> {
+  const storedUserId = localStorage.getItem('userId');
+  const [animeRanking, setAnimeRanking] = useState([]);
+  
+  useEffect(()=> {
+    if (storedUserId) {
+      //fetch ranking from anme quiz
+      
+    }
+  })
   return (
     <div>
         <h1>UserRanking</h1>
