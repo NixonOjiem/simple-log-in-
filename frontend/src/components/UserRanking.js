55a145921682b3a6ad29082e8ce74717eaecacb0
arkstudiodevelopers <nicksonogm@gmail.com>
2025-01-01 20:53:27 +0300

    working on front end

--- frontend/src/components/UserRanking.js
+++ frontend/src/components/UserRanking.js
@@ -1,22 +1,35 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-const UserRanking=()=> {
+const UserRanking = () => {
   const storedUserId = localStorage.getItem('userId');
-  const [animeRanking, setAnimeRanking] = useState([]);
-  
-  useEffect(()=> {
+  const [historyRanking, setHistoryRanking] = useState([]);
+
+  useEffect(() => {
     if (storedUserId) {
-      //fetch ranking from anme quiz
-      
+      // Fetch ranking from history quiz
+      axios.get('/hstory-ranking')
+        .then(response => {
+          setHistoryRanking(response.data);
+        })
+        .catch(error => {
+          console.error('Error fetching ranking data:', error);
+        });
     }
-  })
+  }, [storedUserId]);
+
   return (
     <div>
-        <h1>UserRanking</h1>
-        <p>Lorem</p>
+      <h1>User Ranking</h1>
+      <ul>
+        {historyRanking.map((user, index) => (
+          <li key={index}>
+            {user.username}: {user.score}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default UserRanking
+export default UserRanking;
